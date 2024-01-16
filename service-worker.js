2ad055d2de74e0d4baf654244174eef9fcf52e88
hainamantik <aleks.rassohin@ya.ru>
2024-01-16 19:22:22 +0700

    test redirect

--- service-worker.js
+++ service-worker.js
@@ -3,9 +3,17 @@ self.addEventListener('push', function (event) {
     let message = JSON.parse(event.data.text());
     // Keep the service worker alive until the notification is created.
     event.waitUntil(
-        // Show a notification with title 'ServiceWorker Cookbook' and body 'Alea iacta est'.
         self.registration.showNotification(message.title, {
             body: message.body,
         })
     );
+});
+
+self.addEventListener('notificationclick', function (e) {
+    e.notification.close();
+
+    e.waitUntil(
+        clients.openWindow("https://google.com")
+    );
+
 });
